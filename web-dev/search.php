7693eb05b0d91053a001fb8d236bc49a010c7afd
Wenjing Cao <caow@shernet.sheridancollege.ca>
2019-04-03 20:04:09 -0400

    css
    add background and bootstrap link in index/search/wonder/write php  pages

--- web-dev/search.php
+++ web-dev/search.php
@@ -19,6 +19,24 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
   $stmt3->execute();
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>search page</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<style>
+  body {
+      background-image: url("image/bg.jpg");
+      background-size: 100%;
+  }
+</style>
+<body>
 <section>
   <h1>Aww, sure..</h1>
   <?php $count=$stmt2->fetch();
@@ -34,3 +52,6 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
   <p>content: <?php echo($row["content"]) ?></p>
 <?php } ?>
 </section>
+</body>
+</html>
+
