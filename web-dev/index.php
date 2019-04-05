e4c83e069afc963e9d4b8e0388b3176148de5bd1
[captainaries] <[captainariesz@gmail.com]>
2019-04-05 19:21:46 -0400

    css

--- web-dev/index.php
+++ web-dev/index.php
@@ -21,46 +21,16 @@ $stmt->execute();
 	<!-- <link rel="stylesheet" type="text/css" href="index.css"> -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="css/style.css">
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-<style>
-	body {
-  		background-image: url("image/bg.jpg");
-  		background-size: 100%;
-	}
 
-	.navbar{
-		background-color:#343641;
-		width:100%;
-	}
-
-	.btn-outline-primary {
- 	 	background-color: #dfc277;
-  		color:#343641;
-  		border: 3px solid #6990a2; 
-  		border-radius: 12px;
-  		
-	}
-	.btn{font-weight:bold;}
-
-	.btn-outline-primary:hover {
- 	 	background-color:#6990a2;
- 	 	border: 3px solid #6990a2; 
-	}
-
-	.nav-item{
-		margin-left:10px;
-	}
-
-	
-</style>
 
 <body>
-	
+
 	<header>
 		<nav  class="navbar navbar-light ">
   		<a class="navbar-brand" href="write.php">
@@ -73,7 +43,7 @@ $stmt->execute();
 		</ul>
 		</nav>
 	</header>
-	
+
 
 	<div class = "letters">
 		<h4>Love Letters</h4>
