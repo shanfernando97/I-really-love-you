e6214e247ffbe3934a5620fe5e6c32ef7b06c50c
Wenjing Cao <caow@shernet.sheridancollege.ca>
2019-04-08 22:51:49 -0400

    arrow

---
+++ web-dev/js/hide-write.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+ 
+  // $(".insertBtn").click(function(){
+  //   $("#letter").hide();
+  //   $(".boy-image").hide();
+  // });
+
+  // $(".arrowbutton").click(function(){
+  //   $("#letter").show();
+  //   $(".boy-image").show();
+  // });
+
+  $(".arrowbutton").click(function(){
+			$("#letter").toggle();
+			$(".boy-image").toggle();
+		});
+
+ 
+
+
+
+});
+
+
+
--- web-dev/write.php
+++ web-dev/write.php
@@ -10,13 +10,15 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" media="screen and (max-width: 640px)" href="css/small.css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
 
   <body>
-    <div >
+    
     <header>
 
     <nav  class="navbar navbar-light ">
@@ -33,6 +35,7 @@
     <img class="love-image"src="image/titledark.png" width="50%" alt="love you">
   </header>
 
+    <img class="arrowbutton"src="image/arrowdown.svg" width="2%" alt="arrowbutton">
     <form id="letter">
   		<fieldset class="form-group">
         Hey, <input id="name" class="exampleFormControlTextarea1" type="text" name="name" placeholder="write his/her name" required /><br>
@@ -44,11 +47,13 @@
       </fieldset>
   	</form>
     <img class="boy-image"src="image/boy.svg"  alt="boy">
-
+    
 
 
 
     <script src="js/insert.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/hide-write.js" type="text/javascript" charset="utf-8"></script>
+
   </div>
   </body>
 </html>
