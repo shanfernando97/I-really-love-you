685c797ca48967b026a9c79d4880e7ae4e3bc0d2
[captainaries] <[captainariesz@gmail.com]>
2019-04-09 19:58:40 -0400

    font

--- web-dev/search.php
+++ web-dev/search.php
@@ -30,7 +30,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" media="screen and (max-width: 640px)" href="css/small.css" />
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
 
   	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -80,8 +80,8 @@
 
 	            <div id="letter" class="letter" data-id="<?php echo($row["id"])?>">
 	                <div id="letters">
-	                <p style="font-size:35px; margin-left:20px; margin-top:20px; ">Dear, <?php echo($row["name"]) ?></p>
-	              <p style="font-size:20px; margin-left:20px; margin-top:20px; "><?php echo($row["content"]) ?></p>
+	                <p style="font-size:20px; margin-left:20px; margin-top:20px;font-weight: bold; ">Dear, <?php echo($row["name"]) ?></p>
+	              <p style="font-size:20px; margin-left:20px; margin-top:20px;font-weight: 400; "><?php echo($row["content"]) ?></p>
 	                <p style="font-size:20px;margin-left:20px; margin-top:20px; ">From, <?php echo($row["fromName"]) ?></p>
 	                    <div class="letter-stats">
 												<input id="likes" type="number" name="likes" value="<?php echo($row["likes"]) ?>" hidden=true>
