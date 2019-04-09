cedec9f3f49c045f0fb473a1232c8811f21655e5
Wenjing Cao <caow@shernet.sheridancollege.ca>
2019-04-09 12:30:01 -0400

    css change

--- web-dev/search.php
+++ web-dev/search.php
@@ -1,23 +1,23 @@
-<?php
-$search = $_POST['search'];
+	<?php
+	$search = $_POST['search'];
 
 
-$dsn = "mysql:host=localhost;dbname=mcphesto_iloveyou;charset=utf8mb4";
-$dbusername = "mcphesto_imm";
-$dbpassword = "nhXZ9reEzWuQ";
+	$dsn = "mysql:host=localhost;dbname=mcphesto_iloveyou;charset=utf8mb4";
+	$dbusername = "mcphesto_imm";
+	$dbpassword = "nhXZ9reEzWuQ";
 
-$pdo = new PDO($dsn, $dbusername, $dbpassword);
+	$pdo = new PDO($dsn, $dbusername, $dbpassword);
 
 
 //	$stmt1 = $pdo->prepare("SELECT * FROM `chefs` WHERE `userID` = $id");
-	$stmt = $pdo->prepare("SELECT * FROM `letters` WHERE `name` LIKE '%$search%'");
-  $stmt2 = $pdo->prepare("SELECT COUNT(`name`) FROM `letters` WHERE `name` LIKE '%$search%'");
-  $stmt3 = $pdo->prepare("SELECT * FROM `letters` WHERE `name` LIKE '%$search%'");
+  	$stmt = $pdo->prepare("SELECT * FROM `letters` WHERE `name` LIKE '%$search%'");
+  	$stmt2 = $pdo->prepare("SELECT COUNT(`name`) FROM `letters` WHERE `name` LIKE '%$search%'");
+  	$stmt3 = $pdo->prepare("SELECT * FROM `letters` WHERE `name` LIKE '%$search%'");
 
-  $stmt->execute();
-  $stmt2->execute();
-  $stmt3->execute();
-?>
+  	$stmt->execute();
+  	$stmt2->execute();
+  	$stmt3->execute();
+	?>
 
 <!DOCTYPE html>
 <html>
@@ -32,9 +32,9 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 	<link rel="stylesheet" media="screen and (max-width: 640px)" href="css/small.css" />
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </head>
 
@@ -78,23 +78,25 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 		  while($row=$stmt3->fetch()) {
 				$id = $row["id"];
 		  ?>
+		  <div class="showletters showletterssearch">
 			<div id="letter" class="letter" data-id="<?php echo($row["id"])?>">
 				<div id="letters">
-				<p style="font-size:35px;margin-left:20px;margin-top:20px;">Dear, <?php echo($row["name"]) ?></p>
-			  <p style="font-size:20px;margin-left:20px;margin-top:20px;"><?php echo($row["content"]) ?></p>
-				<p style="font-size:20px;margin-left:20px;margin-top:20px;">From, <?php echo($row["fromName"]) ?></p>
+				<p style="font-size:35px; margin-left:20px; margin-top:20px; ">Dear, <?php echo($row["name"]) ?></p>
+			  <p style="font-size:20px; margin-left:20px; margin-top:20px; "><?php echo($row["content"]) ?></p>
+				<p style="font-size:20px;margin-left:20px; margin-top:20px; ">From, <?php echo($row["fromName"]) ?></p>
 					<div class="letter-stats">
 						<input id="letterID" type="number" name="letterID" value="<?php echo($row["id"]) ?>" hidden=true>
 						<input id="likes" type="number" name="likes" value="<?php echo($row["likes"]) ?>" hidden=true>
 						<input id="poops" type="number" name="poops" value="<?php echo($row["poop"]) ?>" hidden=true>
 
-						<input id="like" style="position:absolute;top: 500px;left: 55px;width:60px;" type="image" src="image/like1.svg" value="<?php echo($row["id"]) ?>">
-						<p id="numLikes" style="position:absolute;top: 510px;left:130px;font-size:35px;"><?php echo($row["likes"]) ?></p>
-						<input id="poop"  style="position:absolute;top:500px;left: 200px;width:60px;"type="image" src="image/poop1.svg">
-						<p id="numPoops" style="position:absolute;top: 510px;left:275px;font-size:35px;"><?php echo($row["poop"]) ?></p>
+						<input id="like" style="position:absolute;top: 250px;left: 55px;width:35px;" type="image" src="image/like1.svg" value="<?php echo($row["id"]) ?>">
+						<p id="numLikes" style="position:absolute;top: 260px;left:130px;font-size:20px;"><?php echo($row["likes"]) ?></p>
+						<input id="poop"  style="position:absolute;top:250px;left: 200px;width:35px;"type="image" src="image/poop1.svg">
+						<p id="numPoops" style="position:absolute;top: 260px;left:275px;font-size:20px;"><?php echo($row["poop"]) ?></p>
 					</div>
 			</div>
 			<?php } ?>
+</div>
 </div>
 <script src="js/like.js" charset="utf-8"></script>
 </body>
